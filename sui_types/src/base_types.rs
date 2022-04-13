543f29c401b089e4791b9820e4dff020c6de726a
George Danezis <george@danez.is>
2022-04-13 15:32:56 +0100

    Rebased to latest

--- sui_types/src/base_types.rs
+++ sui_types/src/base_types.rs
@@ -10,28 +10,21 @@ use ed25519_dalek::Digest;
 
 use hex::FromHex;
 use rand::Rng;
-use serde::{de::Error as _, Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 use std::borrow::Borrow;
-use std::collections::HashSet;
 
 use std::convert::{TryFrom, TryInto};
 use std::fmt;
 
 use crate::readable_serde::BytesOrBase64;
 use crate::readable_serde::BytesOrHex;
-use ed25519_dalek::Digest;
-use hex::FromHex;
+
 use move_core_types::account_address::AccountAddress;
 use move_core_types::ident_str;
 use move_core_types::identifier::IdentStr;
 use opentelemetry::{global, Context};
-use rand::Rng;
 use serde::de::Error;
-use serde::{Deserialize, Serialize};
 use sha3::Sha3_256;
-
-use crate::crypto::PublicKeyBytes;
-use crate::error::SuiError;
 
 #[cfg(test)]
 #[path = "unit_tests/base_types_tests.rs"]
@@ -152,8 +145,9 @@ pub struct TransactionDigest(#[serde(with = "BytesOrBase64")] [u8; TRANSACTION_D
 pub struct ObjectDigest(#[serde(with = "BytesOrBase64")] pub [u8; 32]); // We use SHA3-256 hence 32 bytes here
 
 #[derive(Eq, PartialEq, Ord, PartialOrd, Copy, Clone, Hash, Serialize, Deserialize)]
-pub struct TransactionEffectsDigest(#[serde(with = "BytesOrBase64")] pub [u8; TRANSACTION_DIGEST_LENGTH]);
-
+pub struct TransactionEffectsDigest(
+    #[serde(with = "BytesOrBase64")] pub [u8; TRANSACTION_DIGEST_LENGTH],
+);
 
 pub const TX_CONTEXT_MODULE_NAME: &IdentStr = ident_str!("TxContext");
 pub const TX_CONTEXT_STRUCT_NAME: &IdentStr = TX_CONTEXT_MODULE_NAME;
